cbf37098434dcc9714a36b1a15ed7421cc1b8743
Micah Weston <micah.weston@gmail.com>
2021-10-28 21:53:15 -0500

    Add project files.

---
+++ OrderParserChallenge/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿
+namespace OrderParserChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 33);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(123, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(665, 426);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 221);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 34);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Display Results";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ OrderParserChallenge/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; 
+
+namespace OrderParserChallenge
+{
+    public partial class Form1 : Form
+    {
+        private string filepath;
+
+        // Objects for order data
+        private struct Order
+        {
+            public int OrderNumber;
+            public int TotalItems;
+            public int TotalCost;
+            public string OrderDate;
+            public string CustomerName;
+            public string CustomerPhone;
+            public string CustomerEmail;
+            public bool Paid;
+            public bool Shipped;
+            public bool Completed;
+            public Order(int orderNumber, int totalItems, int totalCost, string orderDate, string customerName, 
+                string customerPhone, string customerEmail, bool paid, bool shipped, bool completed)
+            {
+                OrderNumber = orderNumber;
+                TotalItems = totalItems;
+                TotalCost = totalCost;
+                OrderDate = orderDate;
+                CustomerName = customerName;
+                CustomerPhone = customerPhone;
+                CustomerEmail = customerEmail;
+                Paid = paid;
+                Shipped = shipped;
+                Completed = completed;
+            }
+        }
+        private struct Address
+        {
+
+        }
+        private struct OrderDetails
+        {
+
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            filepath = "";
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        // use the openFileDialog
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult dr = openFileDialog1.ShowDialog();
+
+            if( dr == DialogResult.OK)
+            {
+                filepath = openFileDialog1.FileName;
+            }
+            else
+            {
+                filepath = "";
+            }
+        }
+
+        // Displaying the file
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Clear out old output values
+            richTextBox1.Clear();
+
+            // Verify a valid filepath was selected
+            if( filepath == "")
+            {
+                MessageBox.Show("No file was selected");
+                return;
+            }
+
+            // Open connection to file
+            StreamReader sr = new StreamReader(filepath);
+
+            // Create out file processing loop
+            while( !sr.EndOfStream)
+            {
+                // Read each line
+                string line = sr.ReadLine();
+
+                // Read lines between each category 100, 200, 300
+                string res = line.Substring(0, 1);
+                if(res == "1")
+                {
+                    richTextBox1.AppendText(line + "\n");
+                }
+
+                // richTextBox1.AppendText();
+            }
+
+            // Close connection to file
+            sr.Close();
+
+            // Report results
+        }
+    }
+}
