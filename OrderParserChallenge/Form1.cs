4345c88b0f32e47d4b11aee2ccff279718685022
Micah Weston <micah.weston@gmail.com>
2021-10-30 11:58:06 -0500

    Fixed issues with parsing

--- OrderParserChallenge/Form1.cs
+++ OrderParserChallenge/Form1.cs
@@ -84,12 +84,17 @@ namespace OrderParserChallenge
 
             // Store file into an Enumerable
             string[] lines = File.ReadAllLines(filepath);
+            string previousLoop = "";
             int lineCount = 0;
             int count = 0;
             foreach (string line in lines)
             {
                 if (line.Substring(0, 3) == "100")
                 {
+                    if(previousLoop == "300")
+                    {
+                        count++;
+                    }
                     if (count == 0)
                     {
                         // Saving Order Info
@@ -98,15 +103,14 @@ namespace OrderParserChallenge
                         order0.TotalCost = float.Parse(line.Substring(18, 10).Trim());
                         order0.OrderDate = line.Substring(28, 19).Trim();
                         order0.CustomerName = line.Substring(47, 50).Trim();
-                        order0.CustomerPhone = line.Substring(97, 50).Trim(); // Fix all phone numbers should be (97, 30) not (97, 50)
+                        order0.CustomerPhone = line.Substring(97, 30).Trim(); // Fix all phone numbers should be (97, 30) not (97, 50)
                         order0.CustomerEmail = line.Substring(127, 50).Trim();
                         order0.Paid = line.Substring(177, 1).Equals("1") ? true : false;
                         order0.Shipped = line.Substring(178, 1).Equals("1") ? true : false;
                         order0.Completed = line.Substring(179, 1).Equals("1") ? true : false;
-                        count += 1;
                         lineCount = 0;
                     }
-                    else if (count == 1)
+                    if (count == 1)
                     {
                         // Saving Order Info
                         order1.OrderNumber = line.Substring(3, 10).Trim();
@@ -114,15 +118,14 @@ namespace OrderParserChallenge
                         order1.TotalCost = float.Parse(line.Substring(18, 10).Trim());
                         order1.OrderDate = line.Substring(28, 19).Trim();
                         order1.CustomerName = line.Substring(47, 50).Trim();
-                        order1.CustomerPhone = line.Substring(97, 50).Trim();
+                        order1.CustomerPhone = line.Substring(97, 30).Trim();
                         order1.CustomerEmail = line.Substring(127, 50).Trim();
                         order1.Paid = line.Substring(177, 1).Equals("1") ? true : false;
                         order1.Shipped = line.Substring(178, 1).Equals("1") ? true : false;
                         order1.Completed = line.Substring(179, 1).Equals("1") ? true : false;
-                        count += 1;
                         lineCount = 0;
                     }
-                    else if (count == 2)
+                    if (count == 2)
                     {
                         // Saving Order Info
                         order2.OrderNumber = line.Substring(3, 10).Trim();
@@ -130,12 +133,11 @@ namespace OrderParserChallenge
                         order2.TotalCost = float.Parse(line.Substring(18, 10).Trim());
                         order2.OrderDate = line.Substring(28, 19).Trim();
                         order2.CustomerName = line.Substring(47, 50).Trim();
-                        order2.CustomerPhone = line.Substring(97, 50).Trim();
+                        order2.CustomerPhone = line.Substring(97, 30).Trim();
                         order2.CustomerEmail = line.Substring(127, 50).Trim();
                         order2.Paid = line.Substring(177, 1).Equals("1") ? true : false;
                         order2.Shipped = line.Substring(178, 1).Equals("1") ? true : false;
                         order2.Completed = line.Substring(179, 1).Equals("1") ? true : false;
-                        count += 1;
                         lineCount = 0;
                     }
 
@@ -407,70 +409,141 @@ namespace OrderParserChallenge
                         }
                     }
                 }
+                previousLoop = line.Substring(0, 3);
             }
         }
 
         private void account1_Click(object sender, EventArgs e)
         {
+            string displayAccountOne = $"Order Number: {order0.OrderNumber}\n Total Items: {order0.TotalItems}\n Total Cost: {order0.TotalCost}\n" +
+                   $"Order Date: {order0.OrderDate}\n Customer Name: {order0.CustomerName}\n Customer Phone #: {order0.CustomerPhone}\n" +
+                   $"Customer Email: {order0.CustomerEmail}\n Paid: {order0.Paid}\n Shipped: {order0.Shipped}\n Order Completed: {order0.Completed}\n\n";
+            string displayAccountAddressOne = $"Customer Address: {order0.AddressLineOne}{order0.AddressLineTwo}\n{order0.City}, {order0.OrderState} {order0.ZipCode}\n\n";
+            string errorOne = $"This Account order has an error: {order0.ErrorMessage}\n\n";
+            string displayOrderDetailsOne = "";
+            // validate how many orders
+            if (order0Line0 != null && order0Line1 != null && order0Line2 != null)
+            {
+                displayOrderDetailsOne = $"Order Number: {order0Line0.LineNum}: Quantity: {order0Line0.Quantity}, Cost per item: {order0Line0.CostEach}, " +
+                    $"Total Cost: {order0Line0.TotalItemCost}, Description: {order0Line0.Description}\n\nOrder Number: {order0Line1.LineNum}: Quantity: {order0Line1.Quantity}, " +
+                    $"Cost per item: {order0Line0.CostEach}, Total Cost: {order0Line1.TotalItemCost}, Description: {order0Line1.Description}\n\n" +
+                    $"Order Number: {order0Line2.LineNum}: Quantity: {order0Line2.Quantity}, Cost per item: {order0Line2.CostEach}, Total Cost: {order0Line2.TotalItemCost}" +
+                    $", Description: {order0Line2.Description}\n\n";
+            }
+            else if(order0Line0 != null && order0Line1 != null && order0Line2 == null)
+            {
+                displayOrderDetailsOne = $"Order Number: {order0Line0.LineNum}: Quantity: {order0Line0.Quantity}, Cost per item: {order0Line0.CostEach}, " +
+                    $"Total Cost: {order0Line0.TotalItemCost}, Description: {order0Line0.Description}\n\nOrder Number: {order0Line1.LineNum}: Quantity: {order0Line1.Quantity}, " +
+                    $"Cost per item: {order0Line0.CostEach}, Total Cost: {order0Line1.TotalItemCost}, Description: {order0Line1.Description}\n\n";
+            }
+            else if (order0Line0 != null && order0Line1 == null && order0Line2 == null)
+            {
+                displayOrderDetailsOne = $"Order Number: {order0Line0.LineNum}: Quantity: {order0Line0.Quantity}, Cost per item: {order0Line0.CostEach}, " +
+                    $"Total Cost: {order0Line0.TotalItemCost}, Description: {order0Line0.Description}\n\n";
+            }
+            else
+            {
+                displayOrderDetailsOne = "No order details to be shown.\n\n";
+            }
             richTextBox1.Clear();
             if (order0.ValidOrder == true)
-            {
-                string displayAccountOne = $"Order Number: {order0.OrderNumber}\n Total Items: {order0.TotalItems}\n Total Cost: {order0.TotalCost}\n" +
-                    $"Order Date: {order0.OrderDate}\n Customer Name: {order0.CustomerName}\n Customer Phone #: {order0.CustomerPhone}\n" +
-                    $"Customer Email: {order0.CustomerEmail}\n Paid: {order0.Paid}\n Shipped: {order0.Shipped}\n Order Completed: {order0.Completed}";
-                richTextBox1.AppendText(displayAccountOne);
+            {   
+                richTextBox1.AppendText(displayAccountOne + displayAccountAddressOne + displayOrderDetailsOne);
             }
             else if(order0.ValidOrder == false)
             {
-                string errorOne = $"This Account order has an error: {order0.ErrorMessage}\n\n";
-                richTextBox1.AppendText(errorOne);
-                string displayAccountOne = $"Order Number: {order0.OrderNumber}\n Total Items: {order0.TotalItems}\n Total Cost: {order0.TotalCost}\n" +
-                    $"Order Date: {order0.OrderDate}\n Customer Name: {order0.CustomerName}\n Customer Phone #: {order0.CustomerPhone}\n" +
-                    $"Customer Email: {order0.CustomerEmail}\n Paid: {order0.Paid}\n Shipped: {order0.Shipped}\n Order Completed: {order0.Completed}";
-                richTextBox1.AppendText(displayAccountOne);
+                richTextBox1.AppendText(displayAccountOne + displayAccountAddressOne + displayOrderDetailsOne + errorOne);
             }
 
         }
 
         private void account2_Click(object sender, EventArgs e)
         {
-            richTextBox1.Clear();
-            if (order1.ValidOrder)
-            {
-                string displayAccountTwo = $"Order Number: {order1.OrderNumber}\n Total Items: {order1.TotalItems}\n Total Cost: {order1.TotalCost}\n" +
+            string displayAccountTwo = $"Order Number: {order1.OrderNumber}\n Total Items: {order1.TotalItems}\n Total Cost: {order1.TotalCost}\n" +
                     $"Order Date: {order1.OrderDate}\n Customer Name: {order1.CustomerName}\n Customer Phone #: {order1.CustomerPhone}\n" +
-                    $"Customer Email: {order1.CustomerEmail}\n Paid: {order1.Paid}\n Shipped: {order1.Shipped}\n Order Completed: {order1.Completed}";
-                richTextBox1.AppendText(displayAccountTwo);
+                    $"Customer Email: {order1.CustomerEmail}\n Paid: {order1.Paid}\n Shipped: {order1.Shipped}\n Order Completed: {order1.Completed}\n\n";
+            string displayAccountAddressTwo = $"Customer Address: {order1.AddressLineOne}{order1.AddressLineTwo}\n{order1.City}, {order1.OrderState} {order1.ZipCode}\n\n";
+            string errorTwo = $"This Account order has an error: {order1.ErrorMessage}";
+            string displayOrderDetailsTwo = "";
+            // validate how many orders
+            if (order1Line0 != null && order1Line1 != null && order1Line2 != null)
+            {
+                displayOrderDetailsTwo = $"Order Number: {order1Line0.LineNum}: Quantity: {order1Line0.Quantity}, Cost per item: {order1Line0.CostEach}, " +
+                    $"Total Cost: {order1Line0.TotalItemCost}, Description: {order1Line0.Description}\n\nOrder Number: {order1Line1.LineNum}: Quantity: {order1Line1.Quantity}, " +
+                    $"Cost per item: {order1Line0.CostEach}, Total Cost: {order1Line1.TotalItemCost}, Description: {order1Line1.Description}\n\n" +
+                    $"Order Number: {order1Line2.LineNum}: Quantity: {order1Line2.Quantity}, Cost per item: {order1Line2.CostEach}, Total Cost: {order1Line2.TotalItemCost}" +
+                    $", Description: {order1Line2.Description}\n\n";
+            }
+            else if (order1Line0 != null && order1Line1 != null && order1Line2 == null)
+            {
+                displayOrderDetailsTwo = $"Order Number: {order1Line0.LineNum}: Quantity: {order1Line0.Quantity}, Cost per item: {order1Line0.CostEach}, " +
+                    $"Total Cost: {order1Line0.TotalItemCost}, Description: {order1Line0.Description}\n\nOrder Number: {order1Line1.LineNum}: Quantity: {order1Line1.Quantity}, " +
+                    $"Cost per item: {order1Line0.CostEach}, Total Cost: {order1Line1.TotalItemCost}, Description: {order1Line1.Description}\n\n";
+            }
+            else if (order1Line0 != null && order1Line1 == null && order1Line2 == null)
+            {
+                displayOrderDetailsTwo = $"Order Number: {order1Line0.LineNum}: Quantity: {order1Line0.Quantity}, Cost per item: {order1Line0.CostEach}, " +
+                    $"Total Cost: {order1Line0.TotalItemCost}, Description: {order1Line0.Description}\n\n";
             }
             else
             {
-                string errorTwo = $"This Account order has an error: {order1.ErrorMessage}";
-                richTextBox1.AppendText(errorTwo);
-                string displayAccountTwo = $"Order Number: {order1.OrderNumber}\n Total Items: {order1.TotalItems}\n Total Cost: {order1.TotalCost}\n" +
-                    $"Order Date: {order1.OrderDate}\n Customer Name: {order1.CustomerName}\n Customer Phone #: {order1.CustomerPhone}\n" +
-                    $"Customer Email: {order1.CustomerEmail}\n Paid: {order1.Paid}\n Shipped: {order1.Shipped}\n Order Completed: {order1.Completed}";
-                richTextBox1.AppendText(displayAccountTwo);
+                displayOrderDetailsTwo = "No order details to be shown.\n\n";
+            }
+            richTextBox1.Clear();
+            if (order1.ValidOrder)
+            {
+                
+                richTextBox1.AppendText(displayAccountTwo + displayAccountAddressTwo + displayOrderDetailsTwo);
+            }
+            else
+            {
+                
+                richTextBox1.AppendText(displayAccountTwo + displayAccountAddressTwo + displayOrderDetailsTwo  + errorTwo);
             }
         }
 
         private void Account3_Click(object sender, EventArgs e)
         {
-            richTextBox1.Clear();
-            if (order2.ValidOrder)
-            {
-                string displayAccountThree = $"Order Number: {order2.OrderNumber}\n Total Items: {order2.TotalItems}\n Total Cost: {order2.TotalCost}\n" +
+            string displayAccountThree = $"Order Number: {order2.OrderNumber}\n Total Items: {order2.TotalItems}\n Total Cost: {order2.TotalCost}\n" +
                     $"Order Date: {order2.OrderDate}\n Customer Name: {order2.CustomerName}\n Customer Phone #: {order2.CustomerPhone}\n" +
-                    $"Customer Email: {order2.CustomerEmail}\n Paid: {order2.Paid}\n Shipped: {order2.Shipped}\n Order Completed: {order2.Completed}";
-                richTextBox1.AppendText(displayAccountThree);
+                    $"Customer Email: {order2.CustomerEmail}\n Paid: {order2.Paid}\n Shipped: {order2.Shipped}\n Order Completed: {order2.Completed}\n\n";
+            string displayAccountAddressThree = $"Customer Address: {order2.AddressLineOne}{order2.AddressLineTwo}\n{order2.City}, {order2.OrderState} {order2.ZipCode}\n\n";
+            string errorThree = $"This Account order has an error: {order2.ErrorMessage}";
+            string displayOrderDetailsThree = "";
+            // validate how many orders
+            if (order2Line0 != null && order2Line1 != null && order2Line2 != null)
+            {
+                displayOrderDetailsThree = $"Order Number: {order2Line0.LineNum}: Quantity: {order2Line0.Quantity}, Cost per item: {order2Line0.CostEach}, " +
+                    $"Total Cost: {order2Line0.TotalItemCost}, Description: {order2Line0.Description}\n\nOrder Number: {order2Line1.LineNum}: Quantity: {order2Line1.Quantity}, " +
+                    $"Cost per item: {order2Line0.CostEach}, Total Cost: {order2Line1.TotalItemCost}, Description: {order2Line1.Description}\n\n" +
+                    $"Order Number: {order2Line2.LineNum}: Quantity: {order2Line2.Quantity}, Cost per item: {order2Line2.CostEach}, Total Cost: {order2Line2.TotalItemCost}" +
+                    $", Description: {order2Line2.Description}\n\n";
+            }
+            else if (order2Line0 != null && order2Line1 != null && order2Line2 == null)
+            {
+                displayOrderDetailsThree = $"Order Number: {order2Line0.LineNum}: Quantity: {order2Line0.Quantity}, Cost per item: {order2Line0.CostEach}, " +
+                    $"Total Cost: {order2Line0.TotalItemCost}, Description: {order2Line0.Description}\n\nOrder Number: {order2Line1.LineNum}: Quantity: {order2Line1.Quantity}, " +
+                    $"Cost per item: {order2Line0.CostEach}, Total Cost: {order2Line1.TotalItemCost}, Description: {order2Line1.Description}\n\n";
+            }
+            else if (order2Line0 != null && order2Line1 == null && order2Line2 == null)
+            {
+                displayOrderDetailsThree = $"Order Number: {order2Line0.LineNum}: Quantity: {order2Line0.Quantity}, Cost per item: {order2Line0.CostEach}, " +
+                    $"Total Cost: {order2Line0.TotalItemCost}, Description: {order2Line0.Description}\n\n";
             }
             else
             {
-                string errorThree = $"This Account order has an error: {order2.ErrorMessage}";
-                richTextBox1.AppendText(errorThree);
-                string displayAccountThree = $"Order Number: {order2.OrderNumber}\n Total Items: {order2.TotalItems}\n Total Cost: {order2.TotalCost}\n" +
-                    $"Order Date: {order2.OrderDate}\n Customer Name: {order2.CustomerName}\n Customer Phone #: {order2.CustomerPhone}\n" +
-                    $"Customer Email: {order2.CustomerEmail}\n Paid: {order2.Paid}\n Shipped: {order2.Shipped}\n Order Completed: {order2.Completed}";
-                richTextBox1.AppendText(displayAccountThree);
+                displayOrderDetailsThree = "No order details to be shown.\n\n";
+            }
+            richTextBox1.Clear();
+            if (order2.ValidOrder)
+            {
+                
+                richTextBox1.AppendText(displayAccountThree + displayAccountAddressThree + displayOrderDetailsThree);
+            }
+            else
+            {
+                
+                richTextBox1.AppendText(displayAccountThree + displayAccountAddressThree + displayOrderDetailsThree + errorThree);
             }
         }
     }
